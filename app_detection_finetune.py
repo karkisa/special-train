9f0f6bcf185652dd19e6f70c8fee2c8c65705a83
Sagar Suresh Karki <sagar@Sagars-MacBook-Pro.local>
2023-07-08 00:21:40 -0700

    finetuner for detection app

--- app_detection_finetune.py
+++ app_detection_finetune.py
@@ -5,6 +5,8 @@ import lightning as L
 import streamlit as st
 import cv2, torch, warnings,os
 warnings.filterwarnings('ignore')
+from ipyfilechooser import FileChooser
+from IPython.display import display
 
 class StreamlitApp(L.app.components.ServeStreamlit):
     def build_model(self):
@@ -17,22 +19,26 @@ class StreamlitApp(L.app.components.ServeStreamlit):
         s = s.split('.')
         return s[0]
     
-    def read_vid_and_save_in_folder(self,vid_path,parent_folder = '/nfs/hpc/share/karkisa/AI cap/sturdy-eureka/yolo_training_data/test_data/210914'):
+    def read_vid_and_save_in_folder(self,vid_path,start, end ,thresh,frame_rate, save_folder_path):
+
         vid_ca = cv2.VideoCapture(vid_path)
+        vid_ca.set(cv2.CAP_PROP_FPS, int(frame_rate)) 
         extention = self.get_name_extention(vid_path)
-        save_folder =parent_folder + '/' + extention
+
+        save_folder =save_folder_path + '/' + extention
         if not os.path.exists(save_folder):
             os.mkdir(save_folder)
+
         count = 0
-        time_skips = 3000             # fps is 29.9 .i.e almost 30
+        vid_ca.set(cv2.CAP_PROP_POS_MSEC, 1000*int(start))
+        b = st.button("break")
         while vid_ca.isOpened():
             count+=1
-            t_msec = 1000*(count)
-            vid_ca.set(cv2.CAP_PROP_POS_MSEC, t_msec)
             ret, frame = vid_ca.read()
             name = str(count)+'_'+ extention +'.png'
 
             if ret :
+                st.image(frame)
                 yolo_results = self.model(frame,size = 640)
                 if len(yolo_results.xyxy[0]):
                     x_min,y_min,x_max,y_max,confi,cla = yolo_results.xyxy[0][0].numpy()
@@ -40,21 +46,33 @@ class StreamlitApp(L.app.components.ServeStreamlit):
                         cv2.imwrite(save_folder+'/'+name,frame)
                 
             else : break
+
+            if b:
+                break
+            if count==(int(end)-int(start))*int(frame_rate):
+                break
     
     def render(self):
+        fc = FileChooser()
+        display(fc)
+        
         st.title("Extract Frames and Save important frames")
-        st.subheader("Enter location of the folder that contains videos")
-        vid_folder_path = st.text_input("select sorce folder")
-        vid_folder_path = '/'.join(vid_folder_path.split('/')[:-1])
+        st.subheader("select video")
+        vid_path = st.text_input('Enter folder path', '/Users/sagar/Desktop/AI_cap/sturdy-eureka/data/animated-engine/vid/220904/220904_I2F_S5_U5_DJI0001.MOV')
+
+
         st.subheader("Enter location of folder where you want to save frames with whales")
-        save_folder_path =  st.text_input("select destination folder")
-        save_folder_path = '/'.join(save_folder_path.split('/')[:-1])
-        st.echo(save_folder_path)
+        save_folder_path = st.text_input('Enter folder path', '/Users/sagar/Desktop/AI_cap/deployment/Lightning_Apps/save_detections')
+
+        frame_rate = st.text_input('default reads one frame per second, Sets number of equidistant frames in one second',1)
+        thresh  = st.text_input('Set Detection model threshold',0.8)
+
+        start = st.text_input("start time in seconds")
+        end   = st.text_input("end time in seconds")
+
         if st.button('start'):
-            list_p = os.listdir(vid_folder_path)
-            list_p = [os.path.join(vid_folder_path,p) for p in list_p]
             
-            for vid_path in list_p:
-                self.read_vid_and_save_in_folder(vid_path,save_folder_path)
+            self.read_vid_and_save_in_folder(vid_path,start, end ,thresh,frame_rate, save_folder_path)
+
 
 app = L.LightningApp(StreamlitApp())
