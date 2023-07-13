7fbdb6d08301b428607cb87d4f2c8f039724bb67
Sagar Suresh Karki <sagar@10-249-135-184.wireless.oregonstate.edu>
2023-07-13 12:19:00 -0700

    touches to detection applications

--- app_detection_finetune.py
+++ app_detection_finetune.py
@@ -5,8 +5,6 @@ import lightning as L
 import streamlit as st
 import cv2, torch, warnings,os
 warnings.filterwarnings('ignore')
-from ipyfilechooser import FileChooser
-from IPython.display import display
 
 class StreamlitApp(L.app.components.ServeStreamlit):
     def build_model(self):
@@ -19,7 +17,7 @@ class StreamlitApp(L.app.components.ServeStreamlit):
         s = s.split('.')
         return s[0]
     
-    def read_vid_and_save_in_folder(self,vid_path,start, end ,thresh,frame_rate, save_folder_path):
+    def read_vid_and_save_in_folder(self,vid_path,start, end ,thresh,frame_rate, save_folder_path,display):
 
         vid_ca = cv2.VideoCapture(vid_path)
         extention = self.get_name_extention(vid_path)
@@ -35,10 +33,12 @@ class StreamlitApp(L.app.components.ServeStreamlit):
             count+=1
             vid_ca.set(cv2.CAP_PROP_POS_MSEC, int(factor*count)+ 1000*int(start))
             ret, frame = vid_ca.read()
-            name = str(count)+'_'+ extention +'.png'
+            minutes , seconds = count//60, count%60
+            name = str(minutes)+'_'+str(seconds)+'_'+ extention +'.png'
 
             if ret :
-                st.image(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+                if display:
+                    st.image(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
                 yolo_results = self.model(frame,size = 640)
                 if len(yolo_results.xyxy[0]):
                     x_min,y_min,x_max,y_max,confi,cla = yolo_results.xyxy[0][0].numpy()
@@ -53,22 +53,21 @@ class StreamlitApp(L.app.components.ServeStreamlit):
                 break
     
     def render(self):
-        fc = FileChooser()
-        display(fc)
         
         st.title("Extract Frames and Save important frames")
         st.subheader("Select video")
-        vid_path = st.text_input('Enter folder path', '/Users/sagar/Desktop/AI_cap/sturdy-eureka/data/animated-engine/vid/220904/220904_I2F_S5_U5_DJI0001.MOV')
+        vid_path = st.text_input('Enter video path', '/Users/sagar/Desktop/AI_cap/sturdy-eureka/data/animated-engine/vid/vid/220904/220904_I2F_S5_U5_DJI0001.MOV')
 
 
         st.subheader("Enter location of folder where you want to save frames with whales")
         save_folder_path = st.text_input('Enter folder path', '/Users/sagar/Desktop/AI_cap/deployment/Lightning_Apps/save_detections')
 
-        frame_rate = st.text_input('default reads one frame per second, Sets number of equidistant frames in one second',1)
+        frame_rate = st.text_input('default reads one frame per second',1)
         thresh  = st.text_input('Set Detection model threshold',0.8)
 
         start = st.text_input("start time format : MM-SS")
         end   = st.text_input("end time format : MM-SS")
+        display = st.checkbox("display frames")
         
 
         if st.button('start'):
@@ -77,7 +76,7 @@ class StreamlitApp(L.app.components.ServeStreamlit):
             end  = end.split('-')
             end = int(end[0])*60 + int(end[1])
             st.text(str(start)+ '-' + str(end))
-            self.read_vid_and_save_in_folder(vid_path,start, end ,float(thresh),frame_rate, save_folder_path)
+            self.read_vid_and_save_in_folder(vid_path,start, end ,float(thresh),frame_rate, save_folder_path,display)
 
 
 app = L.LightningApp(StreamlitApp())
